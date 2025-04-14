8bd4ddee734b7e2859ac43c9f96d9ffc6129863a
yeonwoo <mamen7624@gmail.com>
2025-04-14 23:13:07 +0900

    주석수정2

--- wooflix/MovieList/MovieViewController.swift
+++ wooflix/MovieList/MovieViewController.swift
@@ -57,6 +57,7 @@ class MovieViewController: UIViewController {
         subMovieThreeImage.addSubview(flixImage)
     }
     
+    
     func configureLayout() {
         mainMovieView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide)
